754c2ada5d3722356c749646f5a098318217cc37
Minkeisme1204 <Minkeisme1204>
2024-05-14 04:46:06 +0700

    Create basic Folders

---
+++ Cpp Version/lib/intro.cpp
@@ -0,0 +1,9 @@
+#include "intro.hpp"
+#include <iostream>
+#include <chrono>
+#include <windows.h>
+
+int Introduction::display_1() {
+    id = 1; 
+    return id; 
+};
---
+++ Cpp Version/lib/intro.hpp
@@ -0,0 +1,7 @@
+class Introduction {
+    private: 
+        int id = 0; 
+    public: 
+        int display_1();
+        int get_id(); 
+};
