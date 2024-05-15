123068bd7c506bd4ab2a9165212916e7fd4420cb
Minkeisme1204 <Minkeisme1204>
2024-05-15 10:24:36 +0700

    Finish Intro animation

--- Cpp Version/src/main.cpp
+++ Cpp Version/src/main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-using namespace std; 
+#include "../lib/intro.hpp"
 
 int main(int argc, char **argv) {
     while (true) {
-        
+        Introduction *e = new Introduction;  
+        e->loading(); 
+        e->display_1();
+        delete e; 
+        break; 
     }
     return 0; 
 }
